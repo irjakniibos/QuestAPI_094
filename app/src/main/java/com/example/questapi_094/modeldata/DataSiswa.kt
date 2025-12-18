1993e9b49932ef948b5b6a71eda725ed1b03e7a0
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-18 11:51:18 +0700

    Menambahkan model data untuk Siswa

---
+++ app/src/main/java/com/example/questapi_094/modeldata/DataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questapi_094.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
