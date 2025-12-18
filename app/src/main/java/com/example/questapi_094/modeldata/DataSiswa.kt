d2a0b7c3ce1640e7e9b831c1992c8e8a338e612c
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-18 11:53:09 +0700

    Menambahkan data class UIStateSiswa

--- app/src/main/java/com/example/questapi_094/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/questapi_094/modeldata/DataSiswa.kt
@@ -9,3 +9,10 @@ data class DataSiswa(
     val alamat : String,
     val telpon : String
 )
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+
