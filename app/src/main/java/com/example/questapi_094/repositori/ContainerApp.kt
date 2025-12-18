930a90c5bd32bbc8845ac9440f192184e5ed428f
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-18 12:15:09 +0700

    Menambahkan `HttpLoggingInterceptor` ke dalam `DefaultContainerApp` untuk mencatat body respons jaringan. Hal ini berguna untuk proses debugging.

--- app/src/main/java/com/example/questapi_094/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_094/repositori/ContainerApp.kt
@@ -1,6 +1,15 @@
 package com.example.questapi_094.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp{
     val repositoryDataSiswa : RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = ""
+
+    val logging = HttpLoggingInterceptor().apply{
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
