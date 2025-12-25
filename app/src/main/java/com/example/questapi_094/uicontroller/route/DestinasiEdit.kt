6964e7f2bb7bde8135fdcea131d3b461347bed11
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-25 17:10:53 +0700

    Menambahkan judul untuk halaman edit siswa

--- app/src/main/java/com/example/questapi_094/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_094/uicontroller/route/DestinasiEdit.kt
@@ -1,6 +1,11 @@
 package com.example.questapi_094.uicontroller.route
 
+import com.example.questapi_094.R
+
 object DestinasiEdit: DestinasiNavigasi {
     override val route = "item_edit"
+
+    override val titleRes = R.string.edit_siswa
+
 
 }
