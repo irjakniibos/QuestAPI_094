ad67b3b9c8a51911bbfaaab9370bce9901a4a5e7
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-25 17:18:44 +0700

    Menambahkan DetailSiswaScreen

--- app/src/main/java/com/example/questapi_094/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_094/view/HalamanDetail.kt
@@ -1,2 +1,18 @@
 package com.example.questapi_094.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_094.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
