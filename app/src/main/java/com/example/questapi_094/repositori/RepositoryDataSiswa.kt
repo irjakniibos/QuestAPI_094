e9eac481c3906d24ea47a76c6e86e5300e87f1d0
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-25 17:00:23 +0700

    Mendefinisikan fungsi hapusSatuSiswa() pada interface RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_094/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_094/repositori/RepositoryDataSiswa.kt
@@ -11,6 +11,8 @@ interface RepositoryDataSiswa{
     suspend fun getSatuSiswa(id:Int) : DataSiswa
 
     suspend fun editSatuSiswa(id:Int,dataSiswa: DataSiswa) :retrofit2.Response<Void>
+
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
