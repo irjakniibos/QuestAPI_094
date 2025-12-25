57a12722349822affde23fc790cf497b3a6aad21
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-12-25 16:54:45 +0700

    Menambahkan fungsi put untuk mengedit data Siswa

--- app/src/main/java/com/example/questapi_094/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_094/apiservice/ServiceApiSiswa.kt
@@ -4,6 +4,8 @@ import com.example.questapi_094.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa{
     @GET("bacaTeman.php")
@@ -14,4 +16,9 @@ interface ServiceApiSiswa{
 
     @GET("baca1Teman.php/{id}")
     suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+
 }
